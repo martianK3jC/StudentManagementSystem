992e04aee96ec1ddfc3211a5e044b4b3bfa05789
Kesha <kceniza24@gmail.com>
2024-05-26 08:20:35 +0800

    Added sample codes

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(){
-	printf("Kesha Jane Student Management!");
-	return 0;
+int main() {
+    int n, i, j, spaces;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    // Calculate maximum number of spaces needed
+    spaces = n * (n + 1) / 2;
+
+    for (i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (j = 1; j <= spaces - i * (i + 1) / 2; j++) {
+            printf(" ");
+        }
+
+        // Print numbers from 1 to i
+        for (j = 1; j <= i; j++) {
+            printf("%d ", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
