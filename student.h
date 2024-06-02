eac581084f5781dc1cf50702ad23d7f714654656
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-06-03 00:34:21 +0800

    fixed conflicts

--- student.h
+++ student.h
@@ -4,12 +4,12 @@
 #define STUDENT_H
 
 struct Student {
-    char name[50];
-    int age;
-    int id;
-    char degree[50];
-    char address[100];
-    char email[100];
+	char name [50];
+	int age;
+	int id;
+	char degree[50];
+	char address[100];
+	char email[100];
 };
 
 #endif // STUDENT_H
