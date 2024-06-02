06d158bf1f4f9504b1748bb0c2b99241a47e6388
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-06-02 10:01:09 +0800

    maded the script executable

--- update_feature_branches.sh
+++ update_feature_branches.sh
@@ -2,10 +2,21 @@
 
 # Update the local development branch
 git checkout development
+git fetch
 git pull origin development
 
 # Manually define the list of feature branches
-feature_branches=("feature/branch1" "feature/branch2" "feature/branch3")
+feature_branches=(
+    "add-student"
+    "delete-student"
+    "designs-branch"
+    "file-handling"
+    "management-menu"
+    "search-student"
+    "struct-student"
+    "update-student"
+    "view-students"
+)
 
 for branch in "${feature_branches[@]}"; do
     # Checkout the feature branch
