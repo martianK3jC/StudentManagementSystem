f1c5b6ffc217530fe60f19e6139a081b2c0325bc
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-06-03 00:34:12 +0800

    fixed conflicts and added keypressed to continue

--- main.c
+++ main.c
@@ -1,52 +1,11 @@
+#include <windows.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include "student.h"
 #include "manage_student.h"
-#include "mainMenu.h"
-
-void addStudent(struct Student students[], int *studentCount) {
-    struct Student newStudent;
-
-    printf("\n\nEnter student name: ");
-    scanf(" %[^\n]%*c", newStudent.name);
-
-    printf("Enter student age: ");
-    scanf("%d", &newStudent.age);
-
-
-    printf("Enter student ID: ");
-    scanf("%d", &newStudent.id);
-
-    printf("Enter student degree: ");
-    scanf(" %[^\n]%*c", newStudent.degree);
-
-    printf("Enter student address: ");
-    scanf(" %[^\n]%*c", newStudent.address);
-
-    printf("Enter student email: ");
-    scanf("%99s", newStudent.email);
-
-    students[*studentCount] = newStudent;
-    (*studentCount)++;
-
-    printf("Student added successfully!\n");
-}
-
-void viewStudents(struct Student students[], int studentCount) {
-    printf("List of Students:\n\n");
-
-    for (int i = 0; i < studentCount; i++) {
-        printf("Name: %s\n", students[i].name);
-        printf("Age: %d\n", students[i].age);
-        printf("Id: %d\n", students[i].id);
-        printf("Degree: %s\n", students[i].degree);
-        printf("Address: %s\n", students[i].address);
-        printf("Email: %s\n", students[i].email);
-        printf("\n");
-    }
-}
-
-
+#include "ui.h"
 
 int main() {
     struct Student students[100];
@@ -63,6 +22,7 @@ int main() {
         switch (choice) {
             case 1:
                 addStudent(students, &studentCount);
+                
                 break;
             case 2:
                 viewStudents(students, studentCount);
@@ -72,6 +32,8 @@ int main() {
             default:
                 printf("Invalid choice! Please try again.\n");
         }
+        printf("Pressed any key to continue...\n\n");
+        getch();
     }
 
     return 0;
