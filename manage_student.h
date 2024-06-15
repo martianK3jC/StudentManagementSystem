5bf80c0505ad59b78a0a179a71f428489c8adc1a
Kesha <kceniza24@gmail.com>
2024-06-15 20:19:40 +0800

    Updated searchStudent function, fixed some issues, enhanced display appearance

--- manage_student.h
+++ manage_student.h
@@ -1,4 +1,4 @@
-//06-13-2024
+//06-15-2024
 //manage_student.h
 #ifndef MANAGE_STUDENT_H
 #define MANAGE_STUDENT_H
@@ -23,8 +23,8 @@ void addStudent(struct Student students[], int *studentCount) {
     	
         gotoxy(26, 5);
         printf("\033[0;36mADD STUDENT/S\033[0m");
-        gotoxy(25, 6);
-        printf("...............");
+        gotoxy(20, 6);
+        printf("Enter Student Information");
         
         // Input fields
         gotoxy(14, 8);
@@ -42,35 +42,35 @@ void addStudent(struct Student students[], int *studentCount) {
 	    printf("\033[0m\n");
 
         gotoxy(14, 14);
-        printf("Enter student ID:\n");
+        printf("Enter ID:\n");
         gotoxy(14, 15);
 	    printf("\033[0;36m");
 		scanf("%d", &newStudent.id);
 	    printf("\033[0m\n");
         
         gotoxy(14, 17);
-        printf("Enter student degree: \n");
+        printf("Enter degree: \n");
         gotoxy(14, 18);
 	    printf("\033[0;36m");
 		scanf(" %[^\n]%*c", newStudent.degree);
 	    printf("\033[0m\n");
         
         gotoxy(14, 20);
-        printf("Enter student age: \n");
+        printf("Enter age: \n");
         gotoxy(14, 21);
 	    printf("\033[0;36m");
 		scanf("%d", &newStudent.age);
 	    printf("\033[0m\n");
         
         gotoxy(14, 23);
-        printf("Enter student address: \n");
+        printf("Enter address: \n");
         gotoxy(14, 24);
 	    printf("\033[0;36m");
         scanf(" %[^\n]%*c", newStudent.address);
 	    printf("\033[0m\n");
         
    	    gotoxy(14, 26);
-        printf("Enter student email: \n");
+        printf("Enter email: \n");
         gotoxy(14, 27);
 	    printf("\033[0;36m");
 		scanf(" %[^\n]%*c", newStudent.email);
@@ -85,20 +85,18 @@ void addStudent(struct Student students[], int *studentCount) {
             return;
         }
         
-        fprintf(fp, "First Name: %s\n", newStudent.firstName);
-        fprintf(fp, "Last Name: %s\n", newStudent.lastName);
-        fprintf(fp, "Student ID: %d\n", newStudent.id);
-        fprintf(fp, "Degree: %s\n", newStudent.degree);
-        fprintf(fp, "Age: %d\n", newStudent.age);
-        fprintf(fp, "Address: %s\n", newStudent.address);
-        fprintf(fp, "Email: %s\n", newStudent.email);
+        fprintf(fp, "Name    : %s %s\n", newStudent.firstName, newStudent.lastName);
+        fprintf(fp, "ID      : %d\n", newStudent.id);
+        fprintf(fp, "Degree  : %s\n", newStudent.degree);
+        fprintf(fp, "Age     : %d\n", newStudent.age);
+        fprintf(fp, "Address : %s\n", newStudent.address);
+        fprintf(fp, "Email   : %s\n", newStudent.email);
         fprintf(fp, "----------------------------------\n");
 
         fclose(fp);
 
         students[*studentCount] = newStudent;
         (*studentCount)++;
-
 
 	    //success line
         printSAS();
@@ -127,15 +125,11 @@ void addStudent(struct Student students[], int *studentCount) {
 
 	    //press-any key to exit (position)
 	    gotoxy(21,8);
-		printf("Press any key to exit: ");
+    	printf("\033[0;36mPress any key to exit: \033[0m");
 		getch();
 		clearScreen(); 
 
     } while (addAnother == 'y' || addAnother == 'Y');
-    
-    
-    
-    
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -157,7 +151,7 @@ void displayStudents(struct Student students[], int studentCount) {
 	gotoxy(23, 5);
     printf("\033[0;32mLIST OF STUDENTS:\n\n\033[0m");	
 
-	int counter = 8;
+	int counter = 7;
     while (fgets(line, sizeof(line), fp)) {
     	gotoxy(14, counter);
     	
@@ -165,13 +159,77 @@ void displayStudents(struct Student students[], int studentCount) {
     	counter++;
     }
 	
+	fclose(fp);
+	printf("\n");
 	gotoxy(14, counter);
 	printf("\033[1;32mPress any key to exit: \033[0m");
 	getch();
 	clearScreen(); 
-    fclose(fp);
-
 }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+void searchStudent(struct Student students[], int studentCount) {
+    FILE *fp;
+    struct Student info;
+    int idNum, found = 0;
+    
+	searchBox();
+    
+    fp = fopen("student_info.txt", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Can't open file\n");
+        return;
+    }
+    
+    gotoxy(26,4);
+    printf("\033[0;33mSEARCH STUDENT\033[0m");
+	gotoxy(14,8);
+	printf("\033[1;33m");
+    printf("Enter Student Id: ");
+    scanf("%d", &idNum);
+    printf("\033[0m\n");
+    
+    // Note to self: format strings in 'fscanf' functions must match labels in the student_info.txt file
+    while (fscanf(fp, "Name : %s %s\n", info.firstName, info.lastName) == 2) {
+        fscanf(fp, "ID : %d\n", &info.id);
+        fscanf(fp, "Degree       : %[^\n]\n", info.degree);
+        fscanf(fp, "Age          : %d\n", &info.age);
+        fscanf(fp, "Address      : %[^\n]\n", info.address);
+        fscanf(fp, "Email        : %[^\n]\n", info.email);
+        fscanf(fp, "----------------------------------\n");
+
+        if (info.id == idNum) {
+            found = 1;
+			gotoxy(26, 6);
+    		printf("\033[0;32mStudent Found!\033[0m");
+			gotoxy(14, 10);
+            printf("Name    : %s %s", info.firstName, info.lastName);
+            gotoxy(14, 11);
+            printf("ID      : %d", info.id);
+            gotoxy(14,12);
+            printf("Degree  : %s", info.degree);
+            gotoxy(14,13);
+            printf("Age     : %d", info.age);
+            gotoxy(14,14);
+            printf("Address : %s", info.address);
+            gotoxy(14,15);
+            printf("Email   : %s", info.email);
+
+            break; 
+        }
+    }
+
+    if (!found) {
+		gotoxy(24, 10);
+		printf("\033[0;31mStudent Not Found!\033[0m");
+    }
+
+    fclose(fp);
+    printf("\033[0;33m\n\n\t\t     Press any key to exit:\033[0m");    
+	getch();
+	clearScreen(); 
+}
+
 
 
 #endif
