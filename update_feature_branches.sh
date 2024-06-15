1c45e95c828fc427289cb46b66982b5eac25ca5a
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-06-15 21:05:20 +0800

    updated branch script

--- update_feature_branches.sh
+++ update_feature_branches.sh
@@ -2,7 +2,6 @@
 
 #* Update the local development branch
 git checkout development
-git fetch
 git pull origin development
 
 #* Manually define the list of feature branches
@@ -10,13 +9,9 @@ feature_branches=(
     "add-student"
     "delete-student"
     "designs-branch"
-    "file-handling"
-    "management-menu"
-    "search-student"
-    "struct-student"
-    "update-student"
     "view-students"
     "user-interface"
+    "update-delete"
 )
 
 for branch in "${feature_branches[@]}"; do
